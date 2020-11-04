4a91db77b4ea1845db8d14b7fbd479f95df3afbd
Lilita1762 <lilibeth.tabares@gmail.com>
2020-11-04 21:31:40 +0000

    Removing a space

--- console.py
+++ console.py
@@ -162,7 +162,7 @@ class HBNBCommand(cmd.Cmd):
                 else:
                     print("** no instance found **")
             else:
-                 print("** instance id missing **")
+                print("** instance id missing **")
         else:
             print("** class doesn't exist **")
 
